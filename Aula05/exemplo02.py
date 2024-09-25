cf41cc12df2249c0fb87a7cdb852d80cdb175c3b
Manoel974 <maquiavel500@gmail.com>
2024-09-24 21:11:25 -0300

    Atividade aula 5

---
+++ Aula05/exemplo02.py
@@ -0,0 +1,66 @@
+
+# turno = int(input('Informe o número do turno: '))
+
+# match turno:
+#     case 1:
+#         print("Manhã")
+#     case 2:
+#         print('Tarde')
+#     case 3:
+#         print('Noite') 
+#     case _:
+#         print('Turno invalido') 
+
+
+# n = int(input('Digite um número: '))
+
+# match n:
+#     case n if n == 6:
+#         print("Junho")
+#     case n if n == 5:
+#         print('Março')
+#     case n if n == 4:
+#         print('Abril')
+#     case n if n == 3:
+#         print('Maio')
+#     case n if n == 2:
+#         print('Fevereiro')
+#     case n if n == 1:
+#         print('Janeiro')
+
+
+
+# print("\n[1] Chinês \n[2] Italiano \n[3] Mexicano \n[4] Vegetariano")
+
+# n = int(input('Resposta: '))
+ 
+# match n:
+#     case 1:
+#         print('Restaurante Chinês')
+
+
+
+# n = int(input('Digite um numero: '))
+
+
+# print("\n[1] Adicionar Item \n[2] Remover Item \n[3] Listar Itens \n[4] Sair")
+ 
+# p = int(input('Escolher uma opção: '))
+
+
+# match p:
+#     case 1:
+#         print(n+1)
+#     case 2:
+#         print(n-1)
+#     case 3:
+#         print("1 "*n)
+#     case 4:
+#         print('Saindo....')
+        
+
+
+
+        
+
+
